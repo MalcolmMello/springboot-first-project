34d44c8e68dfc0333e991644d0f052fbd959b56d
MalcolmMello <malcolmmellolima@gmail.com>
2023-08-17 10:07:20 -0300

    Subtotal & methods

--- src/main/java/com/educandoweb/course/entities/Payment.java
+++ src/main/java/com/educandoweb/course/entities/Payment.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -25,6 +27,7 @@ public class Payment implements Serializable {
 	
 	@OneToOne
 	@MapsId
+	@JsonIgnore
 	private Order order;
 	
 	public Payment() {}
